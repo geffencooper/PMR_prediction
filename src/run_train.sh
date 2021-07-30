a44080b0c2e21b0f31588da935a2a9bcf5b9cbb7
Geffen Cooper <gcooper@ict.usc.edu>
2021-07-30 15:53:31 -0700

    log working

--- src/run_train.sh
+++ src/run_train.sh
@@ -5,4 +5,4 @@ mkdir ../models/$currDate
 
 fileName="../models/$currDate/$currDate.txt"
 
-python -u network_train.py $currDate | tee $fileName
+python -u network_train.py $currDate | tee -i $fileName
