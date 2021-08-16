f5722a4cb452e9c4fb279e9a92ab47b820a9d537
Geffen Cooper <gcooper@ict.usc.edu>
2021-08-16 14:02:27 -0700

    trying binary set

--- src/run_train.sh
+++ src/run_train.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+# get the current date
 currDate=$(date +%Y-%m-%d_%H-%M-%S)
-mkdir ../models/$currDate
 
+# make this a suffix for the directory name to create a unique directory
+dirName=$1_$currDate
+
+# create the directory
+mkdir ../models/$dirName
+
+# create the log file
 fileName="../models/$1_$currDate/$1.txt"
 
-python -u network_train.py $currDate | tee -i $fileName
+# execute the python training script with the directory passed as input
+# -u means outputs streams are unbuffered, tee sends output streams to a file as well as console,
+# -i ensures that ctrl-c will not break the pipe since we want to save the model on ctrl-c and continue logging
+python -u network_train.py $dirName | tee -i $fileName
