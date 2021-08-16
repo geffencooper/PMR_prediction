0e958c923748928445b4e69d778a9c8aa7030da4
Geffen Cooper <gcooper@ict.usc.edu>
2021-08-16 13:45:28 -0700

    trying binary set

--- src/run_train.sh
+++ src/run_train.sh
@@ -3,6 +3,6 @@
 currDate=$(date +%Y-%m-%d_%H-%M-%S)
 mkdir ../models/$currDate
 
-fileName="../models/$currDate/$currDate.txt"
+fileName="../models/$currDate/$1.txt"
 
 python -u network_train.py $currDate | tee -i $fileName
