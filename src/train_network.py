afacb08dc85b5490bd5b3742bc98b55f0b6f09a6
Geffen Cooper <gcooper@ict.usc.edu>
2021-08-18 11:43:44 -0700

    config file

--- src/train_network.py
+++ src/train_network.py
@@ -211,14 +211,14 @@ def create_optimizer(model,args):
 def create_model(args):
     if args.model_name == "SpeechPaceNN":
         if args.classification == "y":
-            return SpeechPaceNN(args.input_size,args.hidden_size,args.num_layers,args.num_classes,args.gpu_i,args.init_hidden_rand),torch.nn.CrossEntropyLoss()
+            return SpeechPaceNN(args.input_size,args.hidden_size,args.num_layers,args.num_classes,args.gpu_i,args.hidden_init_rand),torch.nn.CrossEntropyLoss()
         elif args.regression == "y":
-            return SpeechPaceNN(args.input_size,args.hidden_size,args.num_layers,-1,args.gpu_i,args.init_hidden_rand),torch.nn.MSELoss()
+            return SpeechPaceNN(args.input_size,args.hidden_size,args.num_layers,-1,args.gpu_i,args.hidden_init_rand),torch.nn.MSELoss()
     elif args.model_name == "PMRfusionModel":
         if args.classification == "y":
-            return PMRfusionNN(args.input_size,args.hidden_size,args.num_layers,args.num_classes,args.gpu_i,args.init_hidden_rand),torch.nn.CrossEntropyLoss()
+            return PMRfusionNN(args.input_size,args.hidden_size,args.num_layers,args.num_classes,args.gpu_i,args.hidden_init_rand),torch.nn.CrossEntropyLoss()
         elif args.regression == "y":
-            return PMRfusionNN(args.input_size,args.hidden_size,args.num_layers,-1,args.gpu_i,args.init_hidden_rand),torch.nn.MSELoss()
+            return PMRfusionNN(args.input_size,args.hidden_size,args.num_layers,-1,args.gpu_i,args.hidden_init_rand),torch.nn.MSELoss()
     else:
         print("ERROR: invalid model name")
         exit(1)
