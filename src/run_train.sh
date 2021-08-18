423471d6db1571d3dcb0c75f5abae7591145004c
Geffen Cooper <gcooper@ict.usc.edu>
2021-08-17 20:47:45 -0700

    config file

--- src/run_train.sh
+++ src/run_train.sh
@@ -1,30 +1,20 @@
 #!/bin/bash
 
-if [[ $1 == "" ]]; then
-    echo "ERROR: Training Session Name not provided as an argument"
-    echo "USAGE: ./run_train.sh [SESSION_NAME] [GPU_INSTANCE]"
-    exit 1
-fi
+source ../config_files/skeleton.conf
+echo $session_name
+# # get the current date
+# currDate=$(date +%Y-%m-%d_%H-%M-%S)
 
-if [[ $2 == "" ]]; then
-    echo "ERRO: GPU instance not provided"
-    echo "USAGE: ./run_train.sh [SESSION_NAME] [GPU_INSTANCE]"
-    exit 1
-fi
+# # make this a suffix for the directory name to create a unique directory
+# dirName=$1_$currDate
 
-# get the current date
-currDate=$(date +%Y-%m-%d_%H-%M-%S)
+# # create the directory
+# mkdir ../models/$dirName
 
-# make this a suffix for the directory name to create a unique directory
-dirName=$1_$currDate
+# # create the log file
+# fileName="../models/$1_$currDate/$1.txt"
 
-# create the directory
-mkdir ../models/$dirName
-
-# create the log file
-fileName="../models/$1_$currDate/$1.txt"
-
-# execute the python training script with the directory passed as input and the gpu instance
-# -u means outputs streams are unbuffered, tee sends output streams to a file as well as console,
-# -i ensures that ctrl-c will not break the pipe since we want to save the model on ctrl-c and continue logging
-python -u network_train.py $dirName $2 | tee -i $fileName
+# # execute the python training script with the directory passed as input and the gpu instance
+# # -u means outputs streams are unbuffered, tee sends output streams to a file as well as console,
+# # -i ensures that ctrl-c will not break the pipe since we want to save the model on ctrl-c and continue logging
+# python -u network_train.py $dirName $2 | tee -i $fileName
