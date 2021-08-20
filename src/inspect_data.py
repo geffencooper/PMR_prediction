0b260d536c3f8528f361ed6bce27c54937ee2981
Geffen Cooper <gcooper@ict.usc.edu>
2021-08-20 12:33:41 -0700

    inspect data

--- src/inspect_data.py
+++ src/inspect_data.py
@@ -8,6 +8,7 @@ are correct
 from pytorch_dataset import SpeechPaceDataset,my_collate_fn,FusedDataset, my_collate_fn_fused
 from network_def import SpeechPaceNN,PMRfusionNN
 import torch
+from torch.utils.data import Dataset, DataLoader
 from torchsampler import ImbalancedDatasetSampler
 import os
 
@@ -20,3 +21,10 @@ val_dataset = FusedDataset(root_dir,os.path.join(root_dir,val_labels_csv))
 
 print(train_dataset.get_dist())
 print(val_dataset.get_dist())
+
+train_loader = DataLoader(train_dataset,32,collate_fn=my_collate_fn_fused,sampler=ImbalancedDatasetSampler(train_dataset))
+val_loader = DataLoader(val_dataset,32,collate_fn=my_collate_fn_fused,sampler=ImbalancedDatasetSampler(val_dataset))
+
+for i,(batch) in train_loader:
+    print(batch)
+    exit()
