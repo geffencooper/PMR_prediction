cd8829cfefd68e64ac23c6fb8ca81c524e8f9faf
Geffen Cooper <gcooper@ict.usc.edu>
2021-08-20 13:18:55 -0700

    inspect data

--- src/inspect_data.py
+++ src/inspect_data.py
@@ -11,6 +11,7 @@ import torch
 from torch.utils.data import Dataset, DataLoader
 from torchsampler import ImbalancedDatasetSampler
 import os
+import pandas as pd
 
 root_dir="/data/perception-working/Geffen/avec_data/"
 train_labels_csv="binary_sampled_train_metadata.csv"
@@ -19,12 +20,26 @@ val_labels_csv="binary_sampled_val_metadata.csv"
 train_dataset = FusedDataset(root_dir,os.path.join(root_dir,train_labels_csv))
 val_dataset = FusedDataset(root_dir,os.path.join(root_dir,val_labels_csv))
 
-print(train_dataset.get_dist())
-print(val_dataset.get_dist())
+# print(train_dataset.get_dist())
+# print(val_dataset.get_dist())
 
-train_loader = DataLoader(train_dataset,32,collate_fn=my_collate_fn_fused,sampler=ImbalancedDatasetSampler(train_dataset))
-val_loader = DataLoader(val_dataset,32,collate_fn=my_collate_fn_fused,sampler=ImbalancedDatasetSampler(val_dataset))
+# train_loader = DataLoader(train_dataset,32,collate_fn=my_collate_fn_fused,sampler=ImbalancedDatasetSampler(train_dataset))
+# val_loader = DataLoader(val_dataset,32,collate_fn=my_collate_fn_fused,sampler=ImbalancedDatasetSampler(val_dataset))
 
-for i,(batch) in enumerate(train_loader):
-    print(batch)
-    exit()
+# for i,(batch) in enumerate(train_loader):
+#     print(batch)
+#     exit()
+
+indices = list(range(len(train_dataset)))
+print("indices:",indices)
+num_samples = len(indices)
+print("num samples:",num_samples)
+df = pd.DataFrame()
+df["label"] = train_dataset.get_labels()
+df.index = indices
+df = df.sort_index()
+print(df)
+label_to_count = df["label"].value_counts()
+print("label to count:",label_to_count)
+weights = 1.0 / label_to_count[df["label"]]
+print("weights:",weights)
