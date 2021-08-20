13d3bfac9af983286d9323379f9b49e64574fd38
Geffen Cooper <gcooper@ict.usc.edu>
2021-08-20 12:30:55 -0700

    inspect data

---
+++ src/inspect_data.py
@@ -0,0 +1,22 @@
+'''
+inspect_data.py 
+
+used to check that datasets, dataloaders, and labels
+are correct
+'''
+
+from pytorch_dataset import SpeechPaceDataset,my_collate_fn,FusedDataset, my_collate_fn_fused
+from network_def import SpeechPaceNN,PMRfusionNN
+import torch
+from torchsampler import ImbalancedDatasetSampler
+import os
+
+root_dir="/data/perception-working/Geffen/avec_data/"
+train_labels_csv="binary_sampled_train_metadata.csv"
+val_labels_csv="binary_sampled_val_metadata.csv"
+
+train_dataset = FusedDataset(root_dir,os.path.join(root_dir,train_labels_csv))
+val_dataset = FusedDataset(root_dir,os.path.join(root_dir,val_labels_csv))
+
+print(train_dataset.get_dist())
+print(val_dataset.get_dist())
