4bef52feb215470237f5fcb59ddcf24e23748f7a
Geffen Cooper <gcooper@ict.usc.edu>
2021-08-20 13:21:46 -0700

    inspect data

--- src/inspect_data.py
+++ src/inspect_data.py
@@ -14,8 +14,8 @@ import os
 import pandas as pd
 
 root_dir="/data/perception-working/Geffen/avec_data/"
-train_labels_csv="binary_sampled_train_metadata.csv"
-val_labels_csv="binary_sampled_val_metadata.csv"
+train_labels_csv="binary_sampled_train_metadata2.csv"
+val_labels_csv="binary_sampled_val_metadata2.csv"
 
 train_dataset = FusedDataset(root_dir,os.path.join(root_dir,train_labels_csv))
 val_dataset = FusedDataset(root_dir,os.path.join(root_dir,val_labels_csv))
@@ -31,7 +31,7 @@ val_dataset = FusedDataset(root_dir,os.path.join(root_dir,val_labels_csv))
 #     exit()
 
 indices = list(range(len(train_dataset)))
-print("indices:",indices)
+#print("indices:",indices)
 num_samples = len(indices)
 print("num samples:",num_samples)
 df = pd.DataFrame()
@@ -43,3 +43,5 @@ label_to_count = df["label"].value_counts()
 print("label to count:",label_to_count)
 weights = 1.0 / label_to_count[df["label"]]
 print("weights:",weights)
+weights = torch.DoubleTensor(weights.to_list())
+print(torch.multinomial(weights,num_samples,replacement=True))
