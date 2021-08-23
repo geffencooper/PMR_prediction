53cff206a1a5c97c2dc39091f4db99ab004ef4d1
Geffen Cooper <gcooper@ict.usc.edu>
2021-08-23 10:50:45 -0700

    inspect data

--- src/inspect_data.py
+++ src/inspect_data.py
@@ -23,13 +23,18 @@ val_dataset = FusedDataset(root_dir,os.path.join(root_dir,val_labels_csv))
 print(train_dataset.get_dist())
 print(val_dataset.get_dist())
 
-train_loader = DataLoader(train_dataset,32,collate_fn=my_collate_fn_fused,sampler=ImbalancedDatasetSampler(train_dataset))
-val_loader = DataLoader(val_dataset,32,collate_fn=my_collate_fn_fused,sampler=ImbalancedDatasetSampler(val_dataset))
+train_loader = DataLoader(train_dataset,32,collate_fn=my_collate_fn_fused,sampler=ImbalancedDatasetSampler(train_dataset)).batch_sampler
+val_loader = DataLoader(val_dataset,32,collate_fn=my_collate_fn_fused,sampler=ImbalancedDatasetSampler(val_dataset)).batch_sampler
 
-for i,(batch) in enumerate(val_loader):
-    print(batch[4])
-    print(batch[5])
-    exit()
+for i, batch_indices in enumerate(train_loader):
+    print(f'Batch #{i} indices: ', batch_indices)
+
+for i, batch_indices in enumerate(val_loader):
+    print(f'Batch #{i} indices: ', batch_indices)
+# for i,(batch) in enumerate(val_loader):
+#     print(batch[4])
+#     print(batch[5])
+#     exit()
 
 # indices = list(range(len(train_dataset)))
 # #print("indices:",indices)
