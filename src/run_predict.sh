27a68cc3624e08627585e72aedf9facb0dd60d6d
Geffen Cooper <gcooper@ict.usc.edu>
2021-08-25 12:45:49 -0700

    predict

--- src/run_predict.sh
+++ src/run_predict.sh
@@ -9,11 +9,12 @@ fi
 
 # include the config file variables
 source $1
+log_dest="none"
 
 # execute the python training script with the directory passed as input and the gpu instance
 # -u means outputs streams are unbuffered, tee sends output streams to a file as well as console,
 # -i ensures that ctrl-c will not break the pipe since we want to save the model on ctrl-c and continue logging
-python -u predict.py $session_name $root_dir --train_data_dir $train_data_dir --val_data_dir $val_data_dir $train_labels_csv \
+python -u predict.py $session_name $log_dest $root_dir --train_data_dir $train_data_dir --val_data_dir $val_data_dir $train_labels_csv \
                            $val_labels_csv $gpu_i $model_name $optim $loss_freq $val_freq \
                            $batch_size $lr $hidden_size $classification $num_classes $regression \
                            $input_size $num_layers $num_epochs $normalize $hidden_init_rand \
