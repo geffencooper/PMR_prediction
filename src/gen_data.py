8fc505476f7aa683a03b455f4d0272576a55b337
Geffen Cooper <gcooper@ict.usc.edu>
2021-08-30 10:22:35 -0700

    new set of models

--- src/gen_data.py
+++ src/gen_data.py
@@ -9,13 +9,32 @@ import pandas as pd
 import numpy as np
 import os
 
-root_dir = "//totoro/perception-working/Geffen/avec_data/"
+root_dir = "/data/perception-working/Geffen/avec_data/"
 train_data_csv = "binary_train_metadata_two_to_one.csv"
 
 labels_df = pd.read_csv(os.path.join(root_dir,train_data_csv))
 num_samples = len(labels_df)
 
-x_train = []
+x_audio_train = []
+x_video_train = []
 y_train = []
-for i in range(num_samples):
-    
+for idx in range(num_samples):
+    patient_id,start,end,label = labels_df.iloc[idx]
+
+    print("read audio ",idx)
+    audio_features = pd.read_csv(os.path.join(root_dir,str(int(patient_id))+"_OpenSMILE2.3.0_mfcc.csv"),sep=";",skiprows=int(start*100),nrows=int((end-start)*100))
+    audio_features = audio_features.iloc[:,np.arange(2,28)]
+    audio_features = (audio_features-audio_features.mean())/audio_features.std()
+
+    print("read video ",idx)
+    visual_features = pd.read_csv(os.path.join(root_dir,str(int(patient_id))+"_OpenFace2.1.0_Pose_gaze_AUs.csv"),sep=",",skiprows=int(start*30),nrows=int((end-start)*30))
+    visual_features = visual_features.iloc[:,np.concatenate((np.arange(4,10),np.arange(18,35)))]
+    visual_features = (visual_features-visual_features.mean())/(visual_features.std() + 0.01)
+
+    x_audio_train.append(audio_features)
+    x_video_train.append(visual_features)
+    y_train.append(label)
+
+print(x_video_train[0])
+print(x_video_train[0])
+print(y_train)
